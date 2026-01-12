d8913fca7529ddde362388013e1ccd9005be9406
David Laurence Aviado <davidaviado.dla@gmail.com>
2026-01-13 02:05:12 +0800

    Feat: Replace movie poster links

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 export const moviesData = [
   {
     genreTags: ["action", "sci-fi"],
-    isClassic: false,
+    isClassic: true,
     title: "The Matrix",
     year: 1999,
     poster:
-      "https://images.unsplash.com/photo-1536440136628-849c177e76a1?w=400",
+      "https://m.media-amazon.com/images/M/MV5BN2NmN2VhMTQtMDNiOS00NDlhLTliMjgtODE2ZTY0ODQyNDRhXkEyXkFqcGc@._V1_.jpg?w=400",
     description: "A computer hacker learns about the true nature of reality.",
-    rating: "8.7/10",
+    rating: "IMDb 8.7/10",
   },
   {
     genreTags: ["drama", "crime"],
@@ -15,10 +15,10 @@ export const moviesData = [
     title: "The Godfather",
     year: 1972,
     poster:
-      "https://images.unsplash.com/photo-1594908900066-3f47337549d8?w=400",
+      "https://m.media-amazon.com/images/M/MV5BNGEwYjgwOGQtYjg5ZS00Njc1LTk2ZGEtM2QwZWQ2NjdhZTE5XkEyXkFqcGc@._V1_.jpg",
     description:
       "The aging patriarch of an organized crime dynasty transfers control.",
-    rating: "9.2/10",
+    rating: "IMDb 9.2/10",
   },
   {
     genreTags: ["action", "adventure"],
@@ -26,10 +26,10 @@ export const moviesData = [
     title: "Mad Max: Fury Road",
     year: 2015,
     poster:
-      "https://images.unsplash.com/photo-1478720568477-152d9b164e26?w=400",
+      "https://m.media-amazon.com/images/M/MV5BZDRkODJhOTgtOTc1OC00NTgzLTk4NjItNDgxZDY4YjlmNDY2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
     description:
       "In a post-apocalyptic wasteland, a woman rebels against tyranny.",
-    rating: "8.1/10",
+    rating: "IMDb 8.1/10",
   },
   {
     genreTags: ["comedy", "romance"],
@@ -37,9 +37,9 @@ export const moviesData = [
     title: "When Harry Met Sally",
     year: 1989,
     poster:
-      "https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?w=400",
+      "https://c8.alamy.com/comp/2JHCM8T/meg-ryan-billy-crystal-poster-when-harry-met-sally-1989-2JHCM8T.jpg",
     description: "Can men and women ever just be friends?",
-    rating: "7.7/10",
+    rating: "IMDb 7.7/10",
   },
   {
     genreTags: ["horror", "thriller"],
@@ -47,9 +47,9 @@ export const moviesData = [
     title: "The Shining",
     year: 1980,
     poster:
-      "https://images.unsplash.com/photo-1509347528160-9a9e33742cdb?w=400",
+      "https://s3.amazonaws.com/nightjarprod/content/uploads/sites/130/2020/09/06231910/Shining-Vertical-scaled.jpg",
     description: "A family heads to an isolated hotel for the winter.",
-    rating: "8.4/10",
+    rating: "IMDb 8.4/10",
   },
   {
     genreTags: ["sci-fi", "adventure"],
@@ -57,9 +57,9 @@ export const moviesData = [
     title: "Interstellar",
     year: 2014,
     poster:
-      "https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?w=400",
+      "https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
     description: "A team of explorers travel through a wormhole in space.",
-    rating: "8.6/10",
+    rating: "IMDb 8.6/10",
   },
   {
     genreTags: ["comedy"],
@@ -67,9 +67,9 @@ export const moviesData = [
     title: "The Grand Budapest Hotel",
     year: 2014,
     poster:
-      "https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?w=400",
+      "https://lumiere-a.akamaihd.net/v1/images/au_20cs_the_grand_budapest_hotel_poster_2fedbe8a.jpeg?region=0%2C0%2C1400%2C2010",
     description: "A legendary concierge and his protege become friends.",
-    rating: "8.1/10",
+    rating: "IMDb 8.1/10",
   },
   {
     genreTags: ["drama", "romance"],
@@ -77,10 +77,10 @@ export const moviesData = [
     title: "Casablanca",
     year: 1942,
     poster:
-      "https://images.unsplash.com/photo-1517604931442-7e0c8ed2963c?w=400",
+      "https://www.shutterstock.com/image-photo/milan-italy-february-11-2019-600nw-2308426979.jpg",
     description:
       "A cynical expatriate American cafe owner struggles to decide.",
-    rating: "8.5/10",
+    rating: "IMDb 8.5/10",
   },
   {
     genreTags: ["thriller", "crime"],
@@ -88,9 +88,9 @@ export const moviesData = [
     title: "Psycho",
     year: 1960,
     poster:
-      "https://images.unsplash.com/photo-1509347528160-9a9e33742cdb?w=400",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Psycho_%281960%29_theatrical_poster_%28retouched%29.jpg/330px-Psycho_%281960%29_theatrical_poster_%28retouched%29.jpg",
     description: "A Phoenix secretary embezzles money from her employer.",
-    rating: "8.5/10",
+    rating: "IMDb 8.5/10",
   },
   {
     genreTags: ["action", "thriller"],
@@ -98,9 +98,9 @@ export const moviesData = [
     title: "John Wick",
     year: 2014,
     poster:
-      "https://images.unsplash.com/photo-1536440136628-849c177e76a1?w=400",
+      "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_FMjpg_UX1000_.jpg",
     description: "An ex-hitman comes out of retirement.",
-    rating: "7.4/10",
+    rating: "IMDb 7.4/10",
   },
   {
     genreTags: ["horror"],
@@ -108,10 +108,10 @@ export const moviesData = [
     title: "Get Out",
     year: 2017,
     poster:
-      "https://images.unsplash.com/photo-1509347528160-9a9e33742cdb?w=400",
+      "https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_.jpg",
     description:
       "A young man uncovers a disturbing secret when visiting his girlfriend's family.",
-    rating: "7.7/10",
+    rating: "IMDb 7.7/10",
   },
   {
     genreTags: ["adventure", "drama"],
@@ -119,8 +119,8 @@ export const moviesData = [
     title: "Lawrence of Arabia",
     year: 1962,
     poster:
-      "https://images.unsplash.com/photo-1485846234645-a62644f84728?w=400",
+      "https://upload.wikimedia.org/wikipedia/commons/4/4d/Lawrence_of_Arabia.jpg",
     description: "The story of T.E. Lawrence during World War I.",
-    rating: "8.3/10",
+    rating: "IMDb 8.3/10",
   },
 ];
